742f8d2e18c359b7dc843c47b452eee3407ca722
Christian Parker <christian.parker@emc.com>
2014-10-24 17:33:09 -0600

    added a count down to cue the person recording

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,8 +18,8 @@ angular.module('cielotimerApp')
             var input = $scope.audio_context.createMediaStreamSource(stream);
             $scope.loggit('Media stream created.');
 
-            input.connect($scope.audio_context.destination);
-            $scope.loggit('Input connected to audio context destination.');
+            //input.connect($scope.audio_context.destination);
+            //$scope.loggit('Input connected to audio context destination.');
 
             $scope.recorder = new Recorder(input);
             $scope.loggit('Recorder initialised.');
@@ -41,29 +41,49 @@ angular.module('cielotimerApp')
             }
 
             if ($scope.currentTimerMS == 0) {
+                $interval.cancel($scope.interval);
                 $scope.done = 'DING!';
+                $scope.play();
             }
         };
 
         $scope.startCountdown = function () {
             $scope.startingMS = parseInt($scope.enteredSeconds) * 1000;
             $scope.currentTimerMS = $scope.startingMS;
-            $interval($scope.updateTime, 100);
+            $scope.interval = $interval($scope.updateTime, 100);
+        };
+
+        $scope.recordCountdown = function () {
+            $scope.countDown -= 1;
+            $scope.recordCue = $scope.countDown;
+            if ($scope.countDown == 0) {
+                $scope.recordCue = 'SPEAK';
+                $interval.cancel($scope.recordCountdownInterval);
+                $scope.recorder.clear();
+                $scope.recorder.record();
+                $scope.loggit('starting record...');
+            }
         };
 
         $scope.startRecord = function () {
-            $scope.recorder.record();
-            $scope.loggit('starting record...');
+            $scope.countDown = 3;
+            $scope.recordCue = $scope.countDown;
+            $scope.recordCountdownInterval = $interval($scope.recordCountdown, 1000);
         };
 
         $scope.stopRecord = function () {
+            $scope.recordCue = '';
             $scope.recorder.stop();
             $scope.loggit('stopping record...');
 
+        };
+
+        $scope.play = function () {
+            $scope.loggit('playing ...');
             $scope.recorder.exportWAV(function (blob) {
                 console.log('got blob ' + blob);
                 var reader = new FileReader();
-                reader.onload = function(someevent) {
+                reader.onload = function (someevent) {
                     console.log('we have an array buf? ' + reader.result);
 
                     $scope.audio_context.decodeAudioData(reader.result, function (audioBuf) {
@@ -77,10 +97,6 @@ angular.module('cielotimerApp')
                 };
                 reader.readAsArrayBuffer(blob);
             });
-        };
-
-        $scope.play = function () {
-            $scope.loggit('playing ...');
         };
 
 
